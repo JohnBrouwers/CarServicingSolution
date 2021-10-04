27e90f9de9f24d6dbbc03ce9905049794e2963e2
John Brouwers <j.brouwers@isential.nl>
2021-10-04 14:07:59 +0200

    Exercise description updated

--- CarServicingConsole/Program.cs
+++ CarServicingConsole/Program.cs
@@ -9,10 +9,13 @@ namespace CarServicingConsole
     {
         static void Main(string[] args)
         {
+            //Exercise:
+            //Enable the garage to have the car serviced by a window repair company
+
             //The program selects the car,
             //  the garage for general maintenance
-            //  and the company for window servicing
-            //The garage should control when window service is performed
+            //  and the company for window servicing.
+            //The garage should control when window service is performed.
 
             var car = new Car("Mercedes");
             var garage = new Garage();
